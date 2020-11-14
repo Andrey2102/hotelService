77273f5b06fd6e7c574fb5f0245853e19711e1e2
Andrey2102 <46657129+Andrey2102@users.noreply.github.com>
2020-11-14 15:12:49 +0200

    Initial

---
+++ Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace hotelService
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
